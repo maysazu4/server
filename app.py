7fdaf1632ebc8d6e8d1da0f5dd67f93a69eeead0
maysazu4 <77391084+maysazu4@users.noreply.github.com>
2024-07-15 16:19:56 +0300

    add get data endpoint

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
 
 app = FastAPI()
-
+load_dotenv()
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+
+@app.get("/data")
+def get_data():
+    return {"message":"data"}
